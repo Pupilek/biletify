efb2b99f7d4bf5be886ff599c05dcc0624aa4bc7
Pupilek <loburets.andrii@gmail.com>
2021-06-16 00:53:11 +0200

    stipe setup

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -3,6 +3,7 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@albiletify/common';
+import { createChargeRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -12,6 +13,7 @@ app.use(
 );
 
 app.use(currentUser);
+app.use(createChargeRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
